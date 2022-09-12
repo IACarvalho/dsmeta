5fee504135e3bc5988c2a0d2443fdba0051a49d7
IACarvalho <israelcarvalhodesenvolvedor@gmail.com>
2022-09-11 21:53:04 -0300

    Send notification

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,8 +1,21 @@
+import axios from 'axios'
 import icon from '../../assets/img/notification-icon.svg'
+import { BASE_URL } from '../../utils/request'
 
 import './styles.css'
 
-export default function NotificationButton() {
+type Props = {
+  saleId: number
+}
+
+function handleClick(id : number) {
+  axios(`${BASE_URL}/sales/${id}/notification`)
+    .then(response => {
+      console.log('sucesso!')
+    })
+}
+
+export default function NotificationButton( {saleId} : Props) {
   return (
     <div className="dsmeta-red-btn">
       <img src={icon} alt="Notificar" />
